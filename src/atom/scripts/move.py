cc36cbe5da7c3b25d9adb551807285819f4d4442
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2023-01-28 14:53:20 -0500

    Update config (steering still broken)

--- src/atom/scripts/move.py
+++ src/atom/scripts/move.py
@@ -3,50 +3,54 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+import curses
+import os
+
 def stop(vel_msg):
-	vel_msg.linear.x = 0
-	vel_msg.linear.y = 0
-	vel_msg.linear.z = 0
-	vel_msg.angular.x = 0
-	vel_msg.angular.y = 0
-	vel_msg.angular.z = 0
+    vel_msg.linear.x = 0
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0
 
-def move():
-	pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-	rospy.init_node('bot_controller', anonymous=True)
-	rate = rospy.Rate(10) # 10hz
 
-	vel_msg = Twist()
-	stop(vel_msg)
+def main(win):
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('bot_controller', anonymous=True)
+    rate = rospy.Rate(10)
+    vel_msg = Twist()
+    stop(vel_msg)
 
-	print("Use w,s,a,d to move the bot and press q for exit")
+    win.nodelay(True)
+    key = ""
+    win.clear()
+    win.addstr("Detected key: ")
 
-	while not rospy.is_shutdown():
-		keyPressed = input()
-		if keyPressed=='q':
-			stop(vel_msg)
-			pub.publish(vel_msg)
-			break
-		elif keyPressed=='w':
-			vel_msg.linear.x = vel_msg.linear.x + 0.01
-
-		elif keyPressed=='a':
-			vel_msg.angular.z = vel_msg.angular.z + 0.01
-
-		elif keyPressed=='s':
-			vel_msg.linear.x = vel_msg.linear.x - 0.01
-
-		elif keyPressed=='d':
-			vel_msg.angular.z = vel_msg.angular.z + 0.01
-
-		elif keyPressed=='x':
-			stop(vel_msg)
-			
-		pub.publish(vel_msg)
-
+    while not rospy.is_shutdown():
+        try:
+            key = win.getkey()
+            win.clear()
+            win.addstr("Detected key: ")
+            win.addstr(str(key))
+            if str(key) == "w":
+                vel_msg.linear.x = 0.5
+            elif str(key) == "s":
+                vel_msg.linear.x = -0.5
+            elif str(key) == "a":
+                vel_msg.angular.z = 2
+            elif str(key) == "d":
+                vel_msg.angular.z = -2
+            elif str(key) == "f":
+                stop(vel_msg)
+            if key == os.linesep:
+                break
+        except Exception as e:
+            pass
+        pub.publish(vel_msg)
 
 if __name__ == '__main__':
-	try:
-		move()
-	except rospy.ROSInterruptException:
-		pass
+    try:
+        curses.wrapper(main)
+    except rospy.ROSInterruptException:
+        pass
