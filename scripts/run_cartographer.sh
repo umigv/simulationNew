e27827642dd59e4e2cd8361d2aab84c8b713c838
Kari Naga <4119937+kari-naga@users.noreply.github.com>
2023-02-05 06:39:34 -0500

    Improve movement and update urdf

--- scripts/run_cartographer.sh
+++ scripts/run_cartographer.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 source devel/setup.zsh
+rosrun xacro xacro src/atom/urdf/atom.xacro > src/atom/urdf/atom.urdf
 roslaunch cartographer_ros my_robot.launch 2> >(grep -v -E 'TF_REPEATED_DATA|buffer_core|lookupTransform')
